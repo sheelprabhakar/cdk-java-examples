c9831aa5e04d84b3af2f35be610aca7feae66507
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2021-09-13 20:45:31 +0530

    Added example foe Minikube on ec2

---
+++ aws-ec2minikube-iac-java/sh/script.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# become root user
+sudo su
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install curl
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+id -nG
+sudo usermod -aG docker ubuntu && newgrp docker
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+minikube version
+sudo minikube start --vm-driver=docker
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
